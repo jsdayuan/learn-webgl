c9bf619b18140145c3387a8984804f07202dd8b1
dayuan-JS <2191516303@qq.com>
2020-01-17 22:26:01 +0800

    联系矩阵与矢量

---
+++ 1t/multiPoint/rotatedTriangle_matrix.js
@@ -0,0 +1,67 @@
+"use strict";
+
+/**
+ * 变换矩阵
+ * 矩阵和矢量的乘法
+ * 使用旋转矩阵重写旋转三角形demo
+ */
+
+const VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  uniform mat4 u_xformMatrix;
+  void main(){
+    gl_Position= u_xformMatrix*a_Position;
+  }
+`;
+
+const FSHADER_SOURCE = `
+  void main(){
+    gl_FragColor=vec4(1.0,0.0,0.0,1.0);
+  }
+`;
+
+const ANGLE = 90.0;
+
+const initBuffer = (gl, a_Position) => {
+  const buffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+  const vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+  gl.enableVertexAttribArray(a_Position);
+};
+
+const main = () => {
+  const webgl = document.getElementById("webgl");
+  const gl = webgl.getContext("webgl");
+
+  initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+
+  const a_Position = gl.getAttribLocation(gl.program, "a_Position");
+
+  initBuffer(gl, a_Position);
+
+  const radian = (Math.PI * ANGLE) / 180;
+
+  const sinB=Math.sin(radian);
+  const cosB=Math.cos(radian);
+
+  const xformMatrix=new Float32Array([
+    cosB,sinB,0.0,0.0,
+    -sinB,cosB,0.0,0.0,
+    0.0,0.0,1.0,0.0,
+    0.0,0.0,0.0,1.0
+  ])
+
+  const u_xformMatrix=gl.getUniformLocation(gl.program,'u_xformMatrix');
+  gl.uniformMatrix4fv(u_xformMatrix,false,xformMatrix)
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+};
